9178f82c8fe905f2ff736dac76ebb42bc5079521
urw7rs <urw.7rs@icloud.com>
2023-10-18 14:29:25 +0900

    add training hyperparameters

--- src/paired/models/hifigan.py
+++ src/paired/models/hifigan.py
@@ -12,19 +12,29 @@ LRELU_SLOPE = 0.1
 
 @dataclass
 class HyperParams:
+    batch_size: int
+    learning_rate: float
+    adam_b1: float
+    adam_b2:float
+    lr_decay: float
+    seed: int
+
     resblock: str
     upsample_rates: List[int]
     upsample_kernel_sizes: List[int]
     upsample_initial_channel: int
     resblock_kernel_sizes: List[int]
     resblock_dilation_sizes: List[List[int]]
+
     segment_size: int
     num_mels: int
     num_freq: int
     n_fft: int
     hop_size: int
     win_size: int
+
     sampling_rate: int
+
     fmin: int
     fmax: int
     fmax_for_loss: Optional[int] = None
