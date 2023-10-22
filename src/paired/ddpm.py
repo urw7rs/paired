a351c94cd93b73cfb32ae5f377253c9377159766
urw7rs <urw.7rs@icloud.com>
2023-10-22 21:01:09 +0900

    equal weights for both modalities

--- src/paired/ddpm.py
+++ src/paired/ddpm.py
@@ -161,7 +161,7 @@ class DDPM(nn.Module):
         x_loss = simple_loss(x_noise, noise_in_x_t)
         y_loss = simple_loss(y_noise, noise_in_y_t)
 
-        total_loss = x_loss + y_loss * 0.01
+        total_loss = x_loss + y_loss
         return total_loss, {"x_loss": x_loss, "y_loss": y_loss}
 
     def sampling_step(self, x_t: Tensor, t: Tensor) -> Tensor:
